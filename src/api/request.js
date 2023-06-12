65d7439bf11192a44b69a024332717d5e403868f
hyx <1541202878@bupt.edu.cn>
2023-06-12 20:16:31 +0800

    将请求等待相应时间改为60s

--- src/api/request.js
+++ src/api/request.js
@@ -9,8 +9,8 @@ const requests = axios.create({
     // 配置对象
     // 基础路径，发送请求的时候，路径当中会出现api
     // baseURL:"/question_box_war",
-    // 代表请求超时的时间5s
-    timeout:5000,
+    // 代表请求超时的时间60s
+    timeout:60000,
 })
 // 请求拦截器：在发送请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
 // requests.interceptors.request.use((config) => {
